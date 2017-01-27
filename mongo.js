cfe9cd8811b1caa29c7edba84dc7a916bcd56bd2
Aleksey Kuzmenko <kurtzpb@gmail.com>
2017-01-27 17:52:24 +0200

    Connection to mongo was added

--- mongo.js
+++ mongo.js
@@ -1,9 +1,21 @@
 var mongo = require('mongodb');
 var assert = require('assert');
-var url = 'mongodb://localhost:27017/user';
+var url = 'mongodb://localhost:27017/node';
+
+var insertUser = function (db, callback) {
+	db.collection('users').insertOne ({
+		'id': 0,
+		'name': 'Aleksey'
+	}, function (err, result) {
+		assert.equal(err, null);
+		console.log('Data was added!');
+		callback();
+	});
+}
 
 mongo.connect(url, function (err, db) {
 	assert.equal(null, err);
-	console.log('Connected to mongodb!');
-	db.close();
+	insertUser(db, function () {
+		db.close();
+	})
 })
